db20f65445256268ab6a2d35a145553d2730e9bc
Cassiano <73121447+CassiaNoTReal@users.noreply.github.com>
2024-11-30 19:52:04 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é VS Code?',
+    'É um editor de texto para programar'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um header?',
+    'É um cabeçalho'
+)
+
+criaCartao(
+    'Lingua Japonesa',
+    'Como se diz "o que?" em Japonês?',
+    '"O que?" em Japonês é "Nani?'
+)
